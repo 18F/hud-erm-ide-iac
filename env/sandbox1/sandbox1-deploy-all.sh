f23afd50ee58cd71c3dff7ccbb87a58b6c7d373f
Leonard Becraft <leonard.becraft@gsa.gov>
2021-01-23 09:26:24 -0500

    update deploy-all scripts

--- env/sandbox1/sandbox1-deploy-all.sh
+++ env/sandbox1/sandbox1-deploy-all.sh
@@ -7,8 +7,3 @@ cd ../microservice
 for d in ./*/ ; do (cd "$d" && terragrunt init); done
 terragrunt apply-all --terragrunt-non-interactive
 
-# cd hs
-# terragrunt apply-all --terragrunt-non-interactive
-
-# cd ../microservice
-# terragrunt apply-all --terragrunt-non-interactive
--- env/sandbox2/sandbox2-deploy-all.sh
+++ env/sandbox2/sandbox2-deploy-all.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 cd mgmt
+for d in ./*/ ; do (cd "$d" && terragrunt init); done
 terragrunt apply-all --terragrunt-non-interactive
 
 cd ../hs
+for d in ./*/ ; do (cd "$d" && terragrunt init); done
+terragrunt apply-all --terragrunt-non-interactive
+
+cd ../microservice
+for d in ./*/ ; do (cd "$d" && terragrunt init); done
 terragrunt apply-all --terragrunt-non-interactive
